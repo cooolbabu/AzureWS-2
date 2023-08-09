ab024a9a86f18a5777d722f7590cb24a1b27cc04
Sreenivas Angara <cooolbabu@yahoo.com>
2023-08-09 08:49:03 -0500

    Event message to Azure function is working

--- ChupAzFunc_ADT/Chup_IoT_ADT_Functions.cs
+++ ChupAzFunc_ADT/Chup_IoT_ADT_Functions.cs
@@ -11,9 +11,11 @@ namespace Chup.iot
     public static class Chup_IoT_ADT_Functions
     {
         [FunctionName("Chup_IoT_ADT_Functions")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        public static void Run([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
         {
+            log.LogInformation("**** Chup_AzFunc_ADT:Chup_IoT_ADT_Functions  *****");
             log.LogInformation(eventGridEvent.Data.ToString());
+            log.LogInformation("**** Chup_AzFunc_ADT:Chup_IoT_ADT_Functions  *****");
         }
     }
 }
