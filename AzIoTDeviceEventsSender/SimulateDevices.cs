a87e825de614def331914b4c05df31c7e331debe
Sreenivas Angara <cooolbabu@yahoo.com>
2023-08-13 16:00:27 -0500

    Saving work

--- AzIoTDeviceEventsSender/SimulateDevices.cs
+++ AzIoTDeviceEventsSender/SimulateDevices.cs
@@ -19,17 +19,26 @@ namespace SimulateDevices
 
 
         //private const string s_connectionString = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=thermostat67;SharedAccessKey=bLlpSITjHHY4DZHkRXhk579nl7HG4KMu0KTE94Yx+SY=";
-        private const string s_connectionString_arm1 = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=Arm1;SharedAccessKey=WGeol59ie14i85GkBCQ9+EbEP9sqno+JwuFXdsQirmA=";
+        private const string s_connectionString_arm1 = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=Arm1;SharedAccessKey=fvGD7JRwPiw/YHKWw8So37nCQlbfsaLVwOF7gGX/P8Y=";
         private const string s_connectionString_arm2 = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=Arm2;SharedAccessKey=WGeol59ie14i85GkBCQ9+EbEP9sqno+JwuFXdsQirmA=";
-        private const string s_connectionString_arm3 = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=Arm3;SharedAccessKey=WGeol59ie14i85GkBCQ9+EbEP9sqno+JwuFXdsQirmA=";
-        private const string s_connectionString_arm4 = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=Arm4;SharedAccessKey=WGeol59ie14i85GkBCQ9+EbEP9sqno+JwuFXdsQirmA=";
+        private const string s_connectionString_arm3 = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=Arm3;SharedAccessKey=WjyB/vYITmxyXn1y9PZUm9SqW2261dC+f5vyYR99M/8=";
+        private const string s_connectionString_arm4 = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=Arm4;SharedAccessKey=flQbJx7R5tVP5CJeZ0rKAWth1wh/dmhVhXhTaF881ow=";
+        private const string s_connectionString_arm5 = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=Arm5;SharedAccessKey=4dtoJqjIqwHYRTChS+wOD8YJpyBo2GDiVYkdN4KcsSQ=";
+        private const string s_connectionString_arm6 = "HostName=adt-101-chupoza.azure-devices.net;DeviceId=Arm6;SharedAccessKey=M7dzws5zOxGBZKKPEqDNH8wW8V13+EK93KyP4K++9ZY=";
 
-        private static DeviceClient s_deviceClient;
+        private static DeviceClient s_deviceClient_arm1;
+        private static DeviceClient s_deviceClient_arm2;
+        private static DeviceClient s_deviceClient_arm3;
+        private static DeviceClient s_deviceClient_arm4;
+        private static DeviceClient s_deviceClient_arm5;
+        private static DeviceClient s_deviceClient_arm6;
+
+        private static string fileName ="";
 
         // Async method to send simulated telemetry
         private static async void SendDeviceToCloudMessagesAsync()
         {
-            JArray deviceData = JArray.Parse(File.ReadAllText(".\\DeviceData\\DeviceData1.json"));
+            JArray deviceData = JArray.Parse(File.ReadAllText(fileName));
             string deviceId = "";
             int i = 1;
 
@@ -48,8 +57,36 @@ namespace SimulateDevices
                     Console.WriteLine("{0}: {1}: Message to: {2} {3}", i, DateTime.Now, deviceId, messageString);
                     //Console.WriteLine(Encoding.UTF8.GetString(message.GetBytes()));
                     // Send the telemetry message
-                    await s_deviceClient.SendEventAsync(message).ConfigureAwait(false);
-                    await Task.Delay(10000).ConfigureAwait(false);
+
+                    switch(deviceId){
+                        case "Arm1":
+                            Console.WriteLine("Sending Message to Arm1");
+                            await s_deviceClient_arm1.SendEventAsync(message).ConfigureAwait(false);
+                            break;
+                        case "Arm2":
+                            Console.WriteLine("Sending Message to Arm2"); 
+                            await s_deviceClient_arm2.SendEventAsync(message).ConfigureAwait(false);
+                            break;
+
+                        case "Arm3":
+                            Console.WriteLine("Sending Message to Arm3"); 
+                            await s_deviceClient_arm3.SendEventAsync(message).ConfigureAwait(false);
+                            break;
+                        case "Arm4":
+                            Console.WriteLine("Sending Message to Arm4"); 
+                            await s_deviceClient_arm4.SendEventAsync(message).ConfigureAwait(false);
+                            break;
+                        case "Arm5":
+                            Console.WriteLine("Sending Message to Arm5"); 
+                            await s_deviceClient_arm5.SendEventAsync(message).ConfigureAwait(false);
+                            break;
+                        case "Arm6":
+                            Console.WriteLine("Sending Message to Arm6"); 
+                            await s_deviceClient_arm6.SendEventAsync(message).ConfigureAwait(false);
+                            break;
+                    }
+                    //
+                    await Task.Delay(3000).ConfigureAwait(false);
                     ++i; // Let's keep a count of messages sent
                 }
             }
@@ -58,12 +95,18 @@ namespace SimulateDevices
                 Console.WriteLine($"Error in ingest function: {ex.Message}");
             }
         }
-        private static void Main()
+        private static void Main(string[] args)
         {
             Console.WriteLine("IoT Hub Quickstarts - Simulated device. Ctrl-C to exit.\n");
+            fileName = args[0];
 
             // Connect to the IoT hub using the MQTT protocol
-            s_deviceClient = DeviceClient.CreateFromConnectionString(s_connectionString_arm2, TransportType.Mqtt);
+            s_deviceClient_arm1 = DeviceClient.CreateFromConnectionString(s_connectionString_arm1, TransportType.Mqtt);
+            s_deviceClient_arm2 = DeviceClient.CreateFromConnectionString(s_connectionString_arm2, TransportType.Mqtt);
+            s_deviceClient_arm3 = DeviceClient.CreateFromConnectionString(s_connectionString_arm3, TransportType.Mqtt);
+            s_deviceClient_arm4 = DeviceClient.CreateFromConnectionString(s_connectionString_arm4, TransportType.Mqtt);
+            s_deviceClient_arm5 = DeviceClient.CreateFromConnectionString(s_connectionString_arm5, TransportType.Mqtt);
+            s_deviceClient_arm6 = DeviceClient.CreateFromConnectionString(s_connectionString_arm6, TransportType.Mqtt);
             SendDeviceToCloudMessagesAsync();
             Console.ReadLine();
 
